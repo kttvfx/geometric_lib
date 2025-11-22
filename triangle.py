968f8d9e53e4d0edd2220ab8872fd3cb87d429d5
kttvfx <oantonalekseevich2@gmail.com>
2025-11-22 15:30:13 +0300

    Merge remote changes

--- triangle.py
+++ triangle.py
@@ -22,4 +22,20 @@ def perimeter(a, b, c):
         raise ValueError("Ошибка, было введено не число")  
     return a + b + c
 
+name: CI/CD LAB №5 Python UnitTests 
+on: [push]
+jobs:
+  test:
+    strategy:
+      matrix:
+        os: [ubuntu-latest, windows-latest]
+    runs-on: ${{ matrix.os }}
+    steps:
+    - name: Set up Python
+      uses: actions/setup-python@v4
+      with:
+        python-version: '3.13.5'
+    - name: Run Python UnitTests
+      run: 
+        python3 -m unittest
 
